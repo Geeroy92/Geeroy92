d641d4e01d6b6bf4c73bbb548f85c4497f81e910
Geeroy92 <85865951+Geeroy92@users.noreply.github.com>
2021-06-16 13:48:04 +0200

    Update cputempv2.py

--- cputempv2.py
+++ cputempv2.py
@@ -1,26 +1,30 @@
 ##############||CPUTemp auslesen und in Variable speichern||##############
+ 
+from time import sleep
 
-with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
-    temp = f.read()
-    temp = float(temp) / 1000
-    temp = round(temp, 2)
-    print(temp)
+while True:
 
-#########################################################################
+    with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+        temp = f.read()
+        temp = float(temp) / 1000
+        temp = round(temp, 2)
+        print(temp)
 
-data = []
+    #########################################################################
 
-with open('/var/www/html/index.html', 'r') as file:
-    for line in file:
-        data.append(line)
-#print(data)
+    data = []
 
-for i, line in enumerate(data):
-    if "CPUTemp =" in line:
-        data[i] = "CPUTemp = " + str(temp) + "\n"
+    with open('/var/www/html/index.html', 'r') as file:
+        for line in file:
+            data.append(line)
+    #print(data)
 
-data = ''.join(data)
-with open('/var/www/html/index.html', 'w') as file:
-    file.write(data)
+    for i, line in enumerate(data):
+        if "CPUTemp =" in line:
+            data[i] = "CPUTemp = " + str(temp) + "\n"
 
+    data = ''.join(data)
+    with open('/var/www/html/index.html', 'w') as file:
+        file.write(data)
 
+    sleep(2)
